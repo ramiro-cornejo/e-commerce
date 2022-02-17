bb252357e55ddadd4d441dcec0b8c59c237e84ea
Ramiro Cornejo <ramiro.s.cornejo@gmail.com>
2022-02-17 10:27:05 -0300

    Detalles de productos

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,9 +1,10 @@
 
+
 export const ItemDetail = ({id, titulo, desc, precio, img, categoria}) => {
     return (
         <div>
             <h3>{titulo}</h3>
-            <img src={"https://via.placeholder.com/150"} alt={categoria}/>
+            <img src={"https://iberpark.com/web/image/product.template/44374/image"} style={{ width: '8rem', margin: '50px' }} alt={categoria}/>
             <p>{desc}</p>
             <h5>Precio: $ {precio}</h5>
 
